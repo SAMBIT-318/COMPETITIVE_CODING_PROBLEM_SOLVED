2011a114009a97aeff1dd27fa1a58f33b112fd7c
SAMBIT SWAIN <sambitswain318@gmail.com>
2026-09-26 20:03:20 +0530

    Update sort-0s-1s-and-2s.py

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.py
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.py
@@ -6,15 +6,12 @@ class Solution:
 
         while mid <= high:
             if arr[mid] == 0:
-                # Swap mid and low, increment both
                 arr[low], arr[mid] = arr[mid], arr[low]
                 low += 1
                 mid += 1
             elif arr[mid] == 1:
-                # 1 is in the correct place, just move mid
                 mid += 1
             else:
-                # Swap mid and high, decrement high
                 arr[mid], arr[high] = arr[high], arr[mid]
                 high -= 1
 
